1bf35f6d6e08bdf0691dd5b8ceffa7afc9640148
Pedro Reimberg <pedro.h.d.r.r@gmail.com>
2023-10-29 12:43:30 -0300

    Update dio exercise 1

--- dio/exercises/1_coffe.js
+++ dio/exercises/1_coffe.js
@@ -1,8 +1,16 @@
 let poteCafe = "Café Pilão"
 let poteAcucar = "Açucar Cristal"
 let poteBiscoito = "Biscoito Maizena"
+const mensagemDaVovo = "Na cozinha da vovó hoje tem: "
 
-console.log("Na cozinha da vovó hoje tem: " +
+console.log(mensagemDaVovo +
+poteCafe + " - " +
+poteAcucar + " - " +
+poteBiscoito + " ")
+
+poteCafe = "Café 3 Corações"
+
+console.log(mensagemDaVovo +
 poteCafe + " - " +
 poteAcucar + " - " +
 poteBiscoito + " ")
