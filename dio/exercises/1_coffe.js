b8b1f59868c28af293fcaa36519f0b70e738d238
Pedro Reimberg <pedro.h.d.r.r@gmail.com>
2023-10-29 12:36:42 -0300

    Exercise 1 commit

---
+++ dio/exercises/1_coffe.js
@@ -0,0 +1,8 @@
+let poteCafe = "Café Pilão"
+let poteAcucar = "Açucar Cristal"
+let poteBiscoito = "Biscoito Maizena"
+
+console.log("Na cozinha da vovó hoje tem: " +
+poteCafe + " - " +
+poteAcucar + " - " +
+poteBiscoito + " ")
