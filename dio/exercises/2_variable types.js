c2ddd40d4a18d0ea8ef03cae1adeb8968d1315f6
Pedro Reimberg <pedro.h.d.r.r@gmail.com>
2023-10-30 18:20:39 -0300

    Exercise 2 update

---
+++ dio/exercises/2_variable types.js
@@ -0,0 +1,17 @@
+//pokemon
+
+let pokemonNome = "Pikachu"
+let pokemonNivel = 20
+let pontosDeVidaPokemon = 45
+let pokemonSexo = "M"
+let pokemonSelecionavel = true
+
+//pessoa
+
+let nomePessoa = "Novo"
+let idade = 99
+let numeroTelefone = "(11)99999-9999"
+let cpf = "999.999.999-99"
+let endereco = "Rua Nova, 9"
+let bairro = "Novo"
+let recebeSalario = false
