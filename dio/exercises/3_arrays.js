9e1a292aa1109a138ef04900cec8c65d9e971ccd
Pedro Reimberg <pedro.h.d.r.r@gmail.com>
2023-10-30 19:08:24 -0300

    Exercise 3 commit

---
+++ dio/exercises/3_arrays.js
@@ -0,0 +1,13 @@
+//array example
+
+let pokemon = ["pikachu" , "charmander" , "bulsasaur"]
+
+console.log(pokemon)
+console.log(pokemon[0])
+console.log(pokemon[1])
+console.log(pokemon[2])
+
+// remove last element
+pokemon.pop()
+
+console.log(pokemon)
