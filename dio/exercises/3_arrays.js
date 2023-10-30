85f356e8a3f08054372da7cf10daf44bfc424ab5
Pedro Reimberg <pedro.h.d.r.r@gmail.com>
2023-10-30 19:24:46 -0300

    Exercise 3 commit

--- dio/exercises/3_arrays.js
+++ dio/exercises/3_arrays.js
@@ -1,6 +1,6 @@
-//array example
+//vector example
 
-let pokemon = ["pikachu" , "charmander" , "bulsasaur"]
+let pokemon = ["Pikachu" , "Charmander" , "Bulsasaur"]
 
 console.log(pokemon)
 console.log(pokemon[0])
@@ -11,3 +11,17 @@ console.log(pokemon[2])
 pokemon.pop()
 
 console.log(pokemon)
+
+//array example
+
+pokemonCollection = [
+    ["Pikachu" , "Male" , "Level 1"], 
+    ["Charmander" , "Female" , "Level 4"],
+    ["Bulsasaur" , "Male" , "Level 1"]
+]
+
+console.log("The pokemon " + pokemonCollection[1][0] + " is " + pokemonCollection[1][2] + " and the gender is " + pokemonCollection[1][1])
+
+//functions
+
+console.log("Array size: " + pokemonCollection.length)
