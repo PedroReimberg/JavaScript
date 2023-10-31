4bec3af970419f2461adea25435c061819d62f24
Pedro Reimberg <pedro.h.d.r.r@gmail.com>
2023-10-31 19:50:01 -0300

    Create exercise 7

---
+++ dio/exercises/7_conditional structures.js
@@ -0,0 +1,32 @@
+//if, else, else if
+
+let name = "Pedro"
+let name2 = "Felipe"
+
+if (name === "Pedro"){
+    console.log("Hello, Pedro!")
+
+}
+else if (name2 === "Felipe"){
+    console.log("Hello, Felipe!")
+}
+else{
+    console.log("Hello")
+}
+
+//life status 3 hearts
+
+let life = 2
+
+if (life <= 0){
+    console.log("You are dead")
+}
+else if (life === 1){
+    console.log("Your life is low")
+}
+else if (life === 2){
+    console.log("Your life is medium")
+}
+else{
+    console.log("Your life is full")
+}
