3c357a715c238e520fa65ca38958dbaa4bac612d
Pedro Reimberg <pedro.h.d.r.r@gmail.com>
2023-10-30 21:39:45 -0300

    Create exercise 4

---
+++ dio/exercises/4_arithmetic operators.js
@@ -0,0 +1,16 @@
+//Mathematical operators
+
+let age = 30
+console.log(age)
+age = 30 + 6
+console.log(age)
+age = 30 - 6
+console.log(age)
+
+let productCode = 1023
+let exactCode = productCode - 1000
+console.log(exactCode)
+
+let productPrice = 100.99
+let taxValue = productCode * 2
+console.log("Tax Value:" + taxValue)
