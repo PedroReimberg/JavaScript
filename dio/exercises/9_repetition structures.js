f378ef1dc19eb77729e618610aa9ca930ccfbfa6
Pedro Reimberg <pedro.h.d.r.r@gmail.com>
2023-11-01 11:43:53 -0300

    Create exercise 9

---
+++ dio/exercises/9_repetition structures.js
@@ -0,0 +1,35 @@
+//for
+
+for (let counter = 0; counter <4; counter++){
+    console.log(counter)
+    console.log("Increasing the counter")
+}
+
+//
+
+let lifePoints = 0
+
+for (let i = 0; i < 10 ; i++){
+    lifePoints +=1
+    console.log("Life potion used")
+}
+
+console.log("Total Life Points: " + lifePoints)
+
+//while
+
+let i = 0
+while(i < 3){
+    console.log("Hello!")
+    i++
+}
+
+//do while
+
+//let i = 0
+i = 0
+
+do{
+    console.log("Hi!")
+    i++
+} while(i < 3)
