5dc23b3f7f3f4fa7e72b3a50988bf44d812d8317
Pedro Reimberg <pedro.h.d.r.r@gmail.com>
2023-11-10 20:57:49 -0300

    Update exercise 12

--- dio/exercises/12_class.js
+++ dio/exercises/12_class.js
@@ -3,9 +3,20 @@ class cakePan{
         this.flavor = flavor
         this.filling = filling
     }
+    write(){
+        console.log(`A delicious ${this.flavor} cake with ${this.filling} filling`)
+    }
+    bake(){
+        console.log(`Baking ${this.flavor} cake`)
+    }
 }
 
 let partyCake = new cakePan("Chocolate", "Nutella")
+let premiumCake = new cakePan("Strawberry", "Chocolate")
 
 console.log(partyCake)
 console.log(partyCake.flavor)
+
+partyCake.write()
+premiumCake.bake()
+premiumCake.write()
