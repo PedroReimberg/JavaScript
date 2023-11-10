2dfa11161641d16df5250e80c11bbcb95e814674
Pedro Reimberg <pedro.h.d.r.r@gmail.com>
2023-11-10 20:29:16 -0300

    Create exercise 12

---
+++ dio/exercises/12_class.js
@@ -0,0 +1,11 @@
+class cakePan{
+    constructor(flavor, filling){
+        this.flavor = flavor
+        this.filling = filling
+    }
+}
+
+let partyCake = new cakePan("Chocolate", "Nutella")
+
+console.log(partyCake)
+console.log(partyCake.flavor)
