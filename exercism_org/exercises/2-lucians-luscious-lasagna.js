9bce8fe1dd8ee591b9f2380ea9e10a5efc81f06f
Pedro Reimberg <pedro.h.d.r.r@gmail.com>
2024-02-17 22:25:08 -0300

    feat: Update Exercise 2

--- exercism_org/exercises/2-lucians-luscious-lasagna.js
+++ exercism_org/exercises/2-lucians-luscious-lasagna.js
@@ -1,11 +1,11 @@
 //URL = https://exercism.org/tracks/javascript/exercises/lasagna/solutions/pedroreimberg
 
-/*
+/**
  * The number of minutes it takes to prepare a single layer.
  */
 const PREPARATION_MINUTES_PER_LAYER = 2;
 export const EXPECTED_MINUTES_IN_OVEN = 40;
-/*
+/**
  * Determines the number of minutes the lasagna still needs to remain in the
  * oven to be properly prepared.
  *
@@ -15,7 +15,7 @@ export const EXPECTED_MINUTES_IN_OVEN = 40;
 export function remainingMinutesInOven(actualMinutesInOven) {
     return EXPECTED_MINUTES_IN_OVEN - actualMinutesInOven
 }
-/*
+/**
  * Given a number of layers, determines the total preparation time.
  *
  * @param {number} numberOfLayers
@@ -24,7 +24,7 @@ export function remainingMinutesInOven(actualMinutesInOven) {
 export function preparationTimeInMinutes(numberOfLayers) {
     return numberOfLayers * PREPARATION_MINUTES_PER_LAYER
 }
-/*
+/**
  * Calculates the total working time. That is, the time to prepare all the layers
  * of lasagna, and the time already spent in the oven.
  *
